242296e789d0b2347fe38723f2af27754559e970
3dogic <yourname@gmail.com>
2025-07-07 20:33:35 +0530

    modfiy city and add gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,17 @@ import Footer from '../components/Footer';
 import LogoLoader from '../components/LogoLoader';
 import '../styles/globals.css';
 import { AuthProvider } from './contexts/AuthContext';
+import { GoogleTagManager, GoogleTagManagerNoScript } from './../components/GoogleTagManage';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <GoogleTagManager />
+      </head>
       <body>
+      <GoogleTagManagerNoScript />
+        {children}
            <AuthProvider>
    <LogoLoader />
         <Header />
