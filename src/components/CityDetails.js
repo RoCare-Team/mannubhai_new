c9e941b7663d19ecd7608e85a53a6d6d1ac3cfd9
ro care <sachhidanand.3dlogic@gmail.com>
2025-07-21 17:51:04 +0530

    trouble maker

--- src/components/CityDetails.js
+++ src/components/CityDetails.js
@@ -47,9 +47,7 @@ const DynamicComponents = {
   AppDownloadCard: dynamic(() => import('@/app/_components/Home/AppDownloadCard'), {
     loading: () => <LoadingPlaceholder className="h-64" />
   }),
-  FloatingContactButtons: dynamic(() => import('@/components/FloatingContactButtons-old'), {
-    ssr: false
-  }),
+ 
 
 };
 
@@ -210,22 +208,9 @@ const CityDetails = ({ city }) => {
           <DynamicComponents.FooterLinks />
         </section>
 
-        <DynamicComponents.ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          toastClassName="!rounded-lg !shadow-md !w-fit !min-w-[200px] !max-w-[80vw] !px-4 !py-2 !text-sm !text-gray-800 !bg-white"
-          bodyClassName="!text-sm"
-        />
+      
       </main>
 
-      <DynamicComponents.FloatingContactButtons />
     </>
   );
 };
